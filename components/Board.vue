402f591912060e733e5da0fbd2b678f8fb558d09
Zachary Svoboda <svobodaz@orvis.com>
2020-03-03 15:07:17 -0500

    init

---
+++ components/Board.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="board-container">
+    <div class="board">
+      <div v-for="location in (rows * columns)" :key="location" class="square" :class="calculateColor(location, columns)" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.component('board', {
+  data () {
+    return {
+      rows: 8,
+      columns: 8
+    }
+  },
+  methods: {
+    calculateColor (location: number, numPerRow: number) {
+      const zeroIndexedLocation = location - 1
+      const alternateRow = (loc: number) => Math.floor(loc / numPerRow) % 2
+      const isDark = (zeroIndexedLocation + alternateRow(zeroIndexedLocation)) % 2 !== 0
+      return {
+        dark: isDark,
+        light: !isDark
+      }
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+$board-dark: rgb(20, 20, 20);
+$board-light: rgb(240, 240, 240);
+
+.board-container {
+  position: relative;
+  width: 100%;
+  padding-bottom: 100%;
+}
+
+.board {
+  border: 2px solid $board-dark;
+  display: grid;
+  grid-template-columns: repeat(8, 1fr);
+  grid-template-columns: repeat(8, 1fr);
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  width: 100%;
+  height: 100%;
+}
+
+.square {
+
+  &.light {
+    background: $board-light;
+  }
+
+  &.dark {
+    background: $board-dark;
+  }
+
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="container">
+    <div class="board-area">
+      <board />
+    </div>
+  </div>
+</template>
+
+<script>
+import Board from '~/components/Board'
+
+export default {
+  components: {
+    Board
+  }
+}
+</script>
+
+<style scoped>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.board-area {
+  width: 80%;
+}
+
+@media screen and (min-width: 800px){
+  .board-area {
+    width: 40%;
+  }
+}
+
+</style>
