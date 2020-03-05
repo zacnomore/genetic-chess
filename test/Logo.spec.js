c315cfe56c87197c364ea13453cd2d4eeb267f98
Zachary Svoboda <svobodaz@orvis.com>
2020-03-05 10:30:57 -0500

    Fix tests

--- test/Logo.spec.js
+++ test/Logo.spec.js
@@ -1,9 +1,9 @@
 import { mount } from '@vue/test-utils'
-import Logo from '@/components/Logo.vue'
+import Board from '@/components/Board.vue'
 
-describe('Logo', () => {
+describe('Board', () => {
   test('is a Vue instance', () => {
-    const wrapper = mount(Logo)
+    const wrapper = mount(Board)
     expect(wrapper.isVueInstance()).toBeTruthy()
   })
 })
